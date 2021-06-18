039ca2bc30ce014e481d8e50dc3516aa56223533
lluvia365 <62463505+lluvia365@users.noreply.github.com>
2021-06-18 18:36:21 +0900

    1

--- inc/main.js
+++ inc/main.js
@@ -1,84 +1,58 @@
 
 
     
-    var w;  // 슬라이드의 가로폭 계산용
+    var slideNo=0;
+    var loop;
     
-    var loop;  // 자동 슬라이드 제어용
-    
-    // 화면 크기에 따라 슬라이드 폭 계산
-    function autoWidth(){
+    // 슬라이드 메인 스크립트
+    function slide(no){
+        slideNo += no;  // 텍스트, 서클버튼용
         
-        // 슬라이드 폭 계산 : 섹션의 3등분(화면에 3장씩 보이게)
-        w = $('#secA').width() / 3;
-        
-        // 700px 미만 화면에서 슬라이드 1장씩 보이게 (100%)
-        if($(window).width() < 700)
-            w = $('#secA').width();
+        if(slideNo > 1) slideNo=0;
+        if(slideNo < 0) slideNo=1;
         
         
+        if(no==1)  // 다음 버튼을 클릭했을 때
+            toLeft();  // 왼쪽으로 이동하는 함수 실행
+        else
+            toRight();  // 오른쪽으로 이동하는 함수 실행
+                
         
-        // 슬라이드 폭 변경(계산된 크기로 맞춤)
-        $('.slide').css('width',w);  // = ('.slide').width(w)
+        // 모든 서클버튼 투명 배경, 현재 서클버튼만 흰색배경
+        $('.circle').css({'background':'transparent','width':'15px'})
+        $('.circle').eq(slideNo).css({'background':'#FFF','width':'30px','border-radius':'20px'});
         
-        // 슬라이드 박스의 폭 변경(슬라이드폭(1장당폭)*개수)
-        $('#slideBox').css('width',w*6);
         
-    };  // autoWidth()
+    };  // 슬라이드 메인 스크립트 slide(no)
+    
+        
     
     
-    // 페이지가 열리거나 창의 크기가 변경될 때 슬라이드 폭 자동 조절하기
-    $(document).ready(autoWidth);
-    $(window).resize(autoWidth);
-    
-    
-    
-    // 페이지 로드/다음 버튼용 슬라이드를 왼쪽으로 이동
+    // 슬라이드를 왼쪽으로 이동하는 함수(다음 버튼용)
     function toLeft(){
-        
-        // 화면의 폭이 700px 이상이면 가운데 슬라이드를 줌 인(확대)
-        // 700px 미만이면 동일한 크기로 맞춤
-        if($(window).width() >= 700){
-        $('.slide').css('transform','scale(.9)');  // 전체 슬라이드 10% 축소후
-        $('.slide').eq(2).css('transform','scale(1.1)');  // 중앙으로 들어올 슬라이드(3번슬라이드) 10% 확대
-        }
-        
-        else
-             $('.slide').css('transform','scale(1)');  // 전체 슬라이드를 원래 크기로 맞춤
-        
-        // 슬라이드 왼쪽으로 진행후 마지막으로 이동
-        $('#slideBox').stop().animate({'margin-left':-w},600,function(){
-            // 첫번째 슬라이드를 슬라이드 박스 맨뒤로 이동
-           $('.slide:first').appendTo('#slideBox');
-            // 슬라이드 박스를 원래위치로 되돌림
-            $('#slideBox').css('margin-left',0);
+        // 1. 슬라이드 박스를 왼쪽으로 이동(헤더 폭만큼)
+        $('#slideBox').stop().animate({'margin-left':'-100%'},1000,function(){
+            // 2. 첫 번째 슬라이드를 슬라이드 박스 맨 뒤로 보내기
+            $('.slide:first').appendTo('#slideBox');
+            // 3. 슬라이드 박스를 원래 위치로 맞춤
+            $('#slideBox').css('margin-left','0');
         });
-    };  // toLeft()
-      
-    
-    // 페이지 로드/이전 버튼용 슬라이드를 오른쪽으로 이동
-    function toRight(){
         
-        // 화면의 폭이 700px 이상이면 가운데 슬라이드를 줌 인(확대)
-        // 700px 미만이면 동일한 크기로 맞춤
-        if($(window).width() >= 700){        
-        
-        $('.slide').css('transform','scale(.9)');  // 전체 슬라이드 10% 축소후
-        $('.slide').eq(0).css('transform','scale(1.1)');  // 중앙으로 들어올 슬라이드(1번슬라이드) 10% 확대
-        }
-        
-        else
-            $('.slide').css('transform','scale(1)');  // 전체 슬라이드를 원래 크기로 맞춤
-        
-        // 마지막 슬라이드를 슬라이드 박스 맨앞으로 이동 후 슬라이드 진행
-        $('.slide:last').prependTo('#slideBox');
-        // 슬라이드 박스를 왼쪽으로 이동(슬라이드 1장값)
-        $('#slideBox').css('margin-left',-w);
-        // 슬라이드 박스를 헤더 안으로(원래위치로) 이동
-        $('#slideBox').stop().animate({'margin-left':0},600);  
     };
-
-
     
+    
+    // 슬라이드를 오른쪽으로 이동하는 함수 (이전 버튼용)
+    function toRight(){
+        //1. 마지막 슬라이드를 박스의 맨 앞으로 가져오기
+        $('.slide:last').prependTo('#slideBox');
+        // 2. 슬라이드 박스를 왼쪽으로 이동(헤더 폭만큼)
+        $('#slideBox').css('margin-left','-100%');
+        // 3. 슬라이드 박스를 오른쪽으로 이동하는 애니메이션
+        $('#slideBox').stop().animate({'margin-left':'0'},1000);
+    };
+    
+    
+     
     $(window).scroll(function(){  // 화면을 스크롤할 때
         // 스크롤 위치가 100픽셀을 넘으면 'top'버튼 보이기
         // 그렇지 않으면(100픽셀 이하) 'top'버튼 숨기기
@@ -92,7 +66,7 @@
 
    
     function playSlide(){
-        loop = setInterval('toLeft()',3000);
+        loop = setInterval('slide(1)',3000);
     }
     
     
@@ -102,27 +76,73 @@
     
 	$(function(){  // 페이지 열릴 때 
 
-		$('.right').click(function(){  // 다음 버튼 클릭
-            // 만약 슬라이드박스가 애니메이션중이 아니면(안움직이면)
-            if(!$('#slideBox').is(':animated'))
-            toLeft();  // 다음 슬라이드 보기 (슬라이드 왼쪽으로 이동)
+		$('.left').click(function(){
+            slide(-1);   
+        });
+                
+		$('.right').click(function(){
+            slide(1);   
         });
         
-		$('.left').click(function(){  // 이전 버튼 클릭
-            // 만약 슬라이드박스가 애니메이션중이 아니면(안움직이면)
-            if(!$('#slideBox').is(':animated'))
-            toRight();  // 이전 슬라이드 보기 (슬라이드 오른쪽으로 이동)
-        });   
+                        
+        $('.circle').click(function(){  // 서클버튼 클릭할 때
+            var no = $(this).index()  // 슬라이드 번호 맞추기
+            slideNo = no;  // 서클버튼의 순번과 슬라이드 번호 맞춤
+            slide(0);  // 슬라이드 실행
+        });
         
         
         $('#gotop').click(function(){  // 'top'버튼 클릭할 때
             // 화면의 스크롤 위치를 맨 상단으로 변경
             $('body,html').stop().animate({'scrollTop':'0'},500)
             
-        });        
+        });     
+        
+        
+        
+        $('.thumb').click(function(){  // 섬네일 이미지 클릭할 때
+            /*
+            1. 모달창 보이기
+            2. 모달창 제목 내용 변경(클릭한 이미지의 alt값)
+            3. 모달창 이미지 변경(클릭한 이미지의 src값)
+            */
+            $('#modal').css('display','flex');
+            
+            $('#modalTitle').html($(this).attr('alt'));
+            $('#modalImage').attr('src',$(this).attr('src'));
+        });
+        
+        
+        $('#modalClose').click(function(){  //모달창 닫기버튼 클릭할 때
+
+            $('#modal').fadeOut(300);  // 모달창 서서히 사라짐
+        });
+        
                 
 
 	});  //$(function()
+
+
+
+    // 화면 해상도별 슬라이드 이미지 변경
+    function autoChange(){
+        if($(window).width() < 500) {  // 화면폭이 500미만일 때
+            $('.slide').eq(0).attr('src','images/mini1.jpg');
+            $('.slide').eq(1).attr('src','images/mini2.jpg');
+            $('.new').attr('src','images/new.png');
+
+        }
+        else {  // 그렇지 않으면(500이상일 때)
+            $('.slide').eq(0).attr('src','images/main1.jpg');
+            $('.slide').eq(1).attr('src','images/main2.jpg');
+            $('.new').attr('src','images/new2.png');
+        }
+    };
+
+    // 창 열릴때, 창크기 조절할때 autoChange 실행하기
+    $(document).ready(autoChange);
+    $(window).resize(autoChange);
+
 
 
 
